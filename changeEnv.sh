86a699bf1bbae76e5127b5d35ae21b1ab645674e
hjpark <hjpark@accupix.com>
2014-06-23 15:03:48 +0900

    Change ENV

--- changeEnv.sh
+++ changeEnv.sh
@@ -6,10 +6,10 @@ mkdir -p ~/env.bak/$CURDATE
 cp -rf ~/.vim ~/env.bak/$CURDATE/
 cp ~/.vimrc ~/env.bak/$CURDATE/
 cp ~/.gitconfig ~/env.bak/$CURDATE/
-
+cp ./.gitconfig ~/.gitconfig
 ln -sf $CURPATH/.vim ~/.vim 
 ln -sf $CURPATH/.vimrc ~/.vimrc
 ln -sf $CURPATH/mkcscope.sh ~/bin/mkcscope.sh
-
+ln -sf $CURPATH/.bashrc ~/.bashrc
 export PATH=$PATH:~/bin
 
